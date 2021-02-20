08947ed12efd89465317d5d4cc5b45d7e0e01024
williamkewei <77704611+williamkewei@users.noreply.github.com>
2021-02-20 23:17:26 +1100

    homework

---
+++ PyBank.py
@@ -0,0 +1,50 @@
+import os
+import csv
+# 'Append file directory and make a complete file path'
+filepath ='budget_data.csv'
+# initialize variables and create lists
+months=[]
+profit_loss_changes =[]
+
+count_months = 0
+net_profit_loss = 0
+previous_month_profit_loss=0
+current_month_profit_loss = 0
+profit_loss_change = 0
+
+# open and read csv file
+with open(filepath, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile,delimiter=",")
+    csv_header= next(csvfile)
+    for row in csv_reader:
+        count_months = count_months+1
+        current_month_profit_loss=int(row[1])
+        net_profit_loss = net_profit_loss + current_month_profit_loss
+
+        if (count_months ==1):
+            previous_month_profit_loss = current_month_profit_loss
+            continue
+        else:
+            profit_loss_change = current_month_profit_loss - previous_month_profit_loss
+            months.append(row[0])
+            profit_loss_changes.append(profit_loss_change)
+            previous_month_profit_loss = current_month_profit_loss
+
+    sum_profit_loss = sum(profit_loss_changes)  
+    average_profit_loss = round(sum_profit_loss/(count_months-1),2)    
+    highest_change = max(profit_loss_changes)   
+    lowest_change = min(profit_loss_changes)   
+    highest_month_index = profit_loss_changes.index(highest_change)
+    lowest_month_index = profit_loss_changes.index(lowest_change)
+    best_month = months[highest_month_index]
+    worst_month = months[lowest_month_index]
+print("financial Analysis")
+print("-----------------")
+print(f"Total Months: {count_months}")
+print(f"Total: ${net_profit_loss}")
+print(f"Average change:{average_profit_loss}")
+print(f"Greatest Increase in profits: {best_month} (${highest_change})")
+print(f"Greatest Decrease in profits: {worst_month} (${lowest_change})")
+
+   
+    
